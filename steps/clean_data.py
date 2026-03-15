fe39863e5a63883da9a4b3d230e33b4795425be3
fahim-506 <fahimp506@gmail.com>
2026-03-15 17:45:56 +0530

    feat: implement data cleaning and data division strategies with error handling and logging

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,7 +1,31 @@
 from utils.logger import logger
 import pandas as pd
 from zenml import step
+from src.data_cleaning import DataCleaning,DataDivideStrategy,DataPreProcessStrategy
+from typing_extensions import Annotated
+from typing import Tuple
 
 @step
-def clean_data(df:pd.DataFrame)->None:
-    pass
+def clean_data(df:pd.DataFrame)->Tuple[
+    Annotated[pd.DataFrame,'X_train'],
+    Annotated[pd.DataFrame,'X_test'],
+    Annotated[pd.Series,'y_train'],
+    Annotated[pd.Series,'y_test']
+]:
+    """Data cleaning class which preprocesses the data and divides it into train and test data.
+
+    Args:
+        data: pd.DataFrame
+    """
+    try:
+        process_strategy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(df,process_strategy)
+        processed_data  = data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data,divide_strategy)
+        X_train,X_test,y_train,y_test = data_cleaning.handle_data()
+        logger.info('Data Cleaning Completed')
+    except Exception as e:
+         logger.error("Error in data cleaning {}".format(e))
+         raise e
