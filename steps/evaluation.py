d991286825a93de3fc19a78967ede807d473db92
fahim-506 <fahimp506@gmail.com>
2026-03-24 01:01:15 +0530

    feat: integrate MLflow for model evaluation and training logging

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -6,7 +6,14 @@ from src.evaluation import MSE,R2,RMSE
 from typing import Tuple
 from typing_extensions import Annotated
 
-@step
+import mlflow
+from zenml.client import Client
+
+experiment_tracker = Client().active_stack.experiment_tracker
+
+logger.info(f"Tracker :: {experiment_tracker}")
+
+@step(experiment_tracker=experiment_tracker.name if experiment_tracker else "TRACKER 124")
 def evaluate_model(model:RegressorMixin,
                    X_test:pd.DataFrame,
                    y_test:pd.Series) -> Tuple[
@@ -23,12 +30,15 @@ def evaluate_model(model:RegressorMixin,
 
         mse_class = MSE()
         mse = mse_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric('mse',mse)
 
         rmse_class = RMSE()
         rmse = rmse_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric('rmse',rmse)
 
         r2_class = R2()
         r2_score = r2_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric('r2_score',r2_score)
 
         return rmse,r2_score
     
